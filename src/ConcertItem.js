19261df4b3b765a884bea5fad52061b7d74bb79a
Þórdís Hildur Þórarinsdóttir <thordishildur@gmail.com>
2019-07-09 22:38:56 +0200

    Add date formatting and hover effect

--- src/ConcertItem.js
+++ src/ConcertItem.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react'
+import './ConcertItem.css';
 
 class ConcertItem extends Component {
 
+    prettyDate (date) {
+        const dateObject =  Date.parse(date)
+        return new Intl.DateTimeFormat('en-GB', { 
+          year: 'numeric', 
+          month: 'long', 
+          day: '2-digit',
+          hour: 'numeric',
+          minute:'numeric'
+        }).format(dateObject)
+    }
 
     render () {
-        return <img src={this.props.concert.imageSource} alt=""/>
+        return (
+        <li className="concertItem">
+            <img src={this.props.concert.imageSource} alt=""/>
+            <div class="eventDescription">
+                <h4>{this.props.concert.eventDateName}</h4>
+                <p>{this.prettyDate(this.props.concert.dateOfShow)}</p>
+            </div>
+        </li>
+        )
     }
 }
 
