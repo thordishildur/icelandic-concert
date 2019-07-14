9505501147eab6811c93c3edd2d29a0b1caa05c9
Þórdís Hildur Þórarinsdóttir <thordishildur@gmail.com>
2019-07-14 23:17:27 +0200

    Updates

---
+++ precache-manifest.c70aafac63885059cf52f7721d8375a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ca726e8a90d74bef2ddd931fe43456",
+    "url": "/icelandic-concert/index.html"
+  },
+  {
+    "revision": "e1422fba4b1e9b6b6fc2",
+    "url": "/icelandic-concert/static/css/2.461d0f21.chunk.css"
+  },
+  {
+    "revision": "9e7a4632bee7e84d9c12",
+    "url": "/icelandic-concert/static/css/main.b6c08fb1.chunk.css"
+  },
+  {
+    "revision": "e1422fba4b1e9b6b6fc2",
+    "url": "/icelandic-concert/static/js/2.f9bf2215.chunk.js"
+  },
+  {
+    "revision": "9e7a4632bee7e84d9c12",
+    "url": "/icelandic-concert/static/js/main.24c502d9.chunk.js"
+  },
+  {
+    "revision": "df2eb1f8dd37a6b33484",
+    "url": "/icelandic-concert/static/js/runtime~main.cb28d0e7.js"
+  }
+]);
